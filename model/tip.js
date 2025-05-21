d1b91d53f48a6a1ce04f87664ce9f5a4bf80d972
Mahedul-Karim <karimrupo21@gmail.com>
2025-05-21 11:50:13 +0600

    tips schema added

---
+++ model/tip.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const tipSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  topic: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  availability: {
+    type: String,
+    required: true,
+    enum: ["Public", "Hidden"],
+  },
+  totalLiked: {
+    type: Number,
+    default: 0,
+  },
+  email: {
+    type: String,
+  },
+});
+
+const Tip = mongoose.model("Tip", userSchema);
+
+module.exports = User;
