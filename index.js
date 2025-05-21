1e726202ddadc7e9b26786874077779c927ae5cf
Mahedul-Karim <karimrupo21@gmail.com>
2025-05-21 10:37:46 +0600

    user schema and error controller added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config({ path: "./.env.local" });
 
 const { connectDB } = require("./config/db");
+const { handleError } = require("./controller/error");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
